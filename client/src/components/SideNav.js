4babd2cbc401fb22aea319c87e4733107c6f8762
Fozg <phongdaohong@gmail.com>
2018-12-05 16:39:55 +0700

    Styling

---
+++ client/src/components/SideNav.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link, Route } from "react-router-dom";
+
+import styles from './SideNav.module.scss';
+
+export default class SideNav extends React.Component {
+  state = {
+    blog: ""
+  }
+
+  componentDidMount () {
+    
+  }
+
+  render () {
+    const {
+      match
+    } = this.props;
+    
+    return (
+      <div>
+        <h3>Other blogs</h3>
+          <h3>{match && match.params.slug}</h3>
+          <ul className={styles.sideNavList}>
+            <li><Link to="/blog/blog1">Blog 1</Link></li>
+            <li><Link to="/blog/javascript">javascript</Link></li>
+          </ul>
+              
+      </div>
+    )
+  }
+}
