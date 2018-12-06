fc10ba82267e3c42b5e3c92a50f0ec6b1dbc35f5
Fozg <phongdaohong@gmail.com>
2018-12-06 15:52:36 +0700

    Update topnav style

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -9,7 +9,7 @@ export default class TopNav extends React.Component {
     return (
       <div className={styles.topnav}>
         <div className="container">
-          <img src={isProduction ? "/public/f-read.png": "/f-read.png"} className={styles.logo} alt="f/Read"></img>
+          <img src={isProduction ? "/public/f-read-box.png": "/f-read-box.png"} className={styles.logo} alt="f/Read"></img>
         </div>
       </div>
     )
