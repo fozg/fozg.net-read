641469fcd656be4bd875c792f3467252c7ba390a
Fozg <phongdaohong@gmail.com>
2018-12-06 12:17:38 +0700

    Add author box, avatar large comp

--- client/src/components/AuthorBox.js
+++ client/src/components/AuthorBox.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import Avatar from './Avatar';
+
 import styles from './AuthorBox.module.scss';
 
 export default class AuthorBox extends React.Component {
 
   render () {
+    const {author} = this.props;
+
     return (
       <div className={styles.authorBox}>
-        Fozg [follow]
+        <Avatar {...author} size="large" />
       </div>
     )
   }
--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import cx from 'classnames';
+import moment from 'moment';
 
 import styles from './Avatar.module.scss';
 
+import GitHubIcon from '../static/GitHub-Mark-32px.png';
+
 export default ({
-  imageUrl,
+  profileImageUrl,
   name,
   title,
+  joined,
   size = 'medium' // [small, medium, large]
 }) => (
-  <div className={styles.avatar}>
-    <img src={imageUrl} className={cx(styles.avtImage, styles[size])}></img>
-    <strong className={styles.name}>{name}</strong>
+  <div className={styles.avatar} style={{flexDirection: size === 'large' ? 'column' : 'row'}}>
+    <img src={profileImageUrl} className={cx(styles.avtImage, styles[size])}></img>
+    <div className={styles.namensocial}>
+      <strong className={styles.name}>{name}</strong>
+      {size === 'large' &&
+        <div>
+          <a href="https://github.com/fozg" target="_blank" className={styles.socialIconLink}>
+            <img src={GitHubIcon} className={cx(styles.socialIcon, styles[`socialIcon-${size}`])} />
+          </a>
+        </div>
+      }
+    </div>
+    {size === 'large' && <pre>{title}</pre>}
+    {size === 'large' && <div className={styles.joined}>
+      Joined: {moment(joined).format('MMM DD YYYY')}
+    </div>}
+    
   </div>
 )
