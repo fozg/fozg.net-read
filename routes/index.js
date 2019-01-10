0b10de41d1a00424244edbc4ecf06b64bf309f91
Fozg <phongdaohong@gmail.com>
2019-01-11 00:58:07 +0700

    Update ENV to test on prod

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-let hostName = process.env.NODE_ENV === 'production' ? '' : '/blog'
+let hostName = process.env.NODE_ENV === 'production' ? '/' : '/blog'
 
 router.get(hostName, require('./blog'));
 router.get(hostName + '/:slug', require('./blog-slug'));
