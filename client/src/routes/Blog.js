c0271bf3dad8962c77da42853e5ab5d0a977ea22
Phong Dao <phongdaohong@gmail.com>
2019-02-28 17:21:32 +0700

    Update Blog.js

--- client/src/routes/Blog.js
+++ client/src/routes/Blog.js
@@ -1,44 +1,56 @@
-import React from 'react'
-
-import SideNav from '../components/SideNav';
-import BlogContent from '../components/BlogContent';
-import AuthorBox from '../components/AuthorBox';
-import Conversations from '../components/Conversations';
+import React from 'react';
+import moment from 'moment';
+import {Link} from 'react-router-dom';
+import {callAPI} from '../lib';
+import {API} from '../consts';
 
 import styles from './Blog.module.scss';
 
-export default class RenderBlog extends React.Component {
-  // state = {
-  //   isBlogLoaded : false
-  // }
+export default class Blog extends React.Component {
   state = {
-    blog: null
+    blogs: []
   }
 
-  _onBlogLoaded = blog => {
-    this.setState({blog})
+  componentDidMount () {
+    // TODO: hard code for now
+    new callAPI(API.BLOGS_BY_USER , "GET", {username: "fozg"}).call().then(
+      blogs => {
+        this.setState({blogs})
+      }
+    )
   }
 
   render () {
-    const slug = this.props.match.params.slug;
-    const {blog} = this.state;
+    const {
+      blogs
+    } = this.state;
 
     return (
-      <div>
-        <div className="container">
-          <div className="row">
-            <div className="blogWrap">
-              <BlogContent slug={slug} key={slug} onBlogLoaded={this._onBlogLoaded}/>
-            </div>
-            <div className={styles.sidebar}>
-              <AuthorBox author={blog ? blog.author : null} />
-              
-              <SideNav />
+      <div className="container">
+        {blogs.map((blog) => (
+          <div key={blog.slug} className={styles.blogItem}>
+            <span className={styles.time}>{moment(blog.created).fromNow()}</span>
+            <Link to={`/blog/${blog.slug}`}>
+              <h1 className={styles.blogTitle}>{blog.title}</h1>
+            </Link>
+            <span className={styles.blogDescription}>{blog.description}</span>
+            <div className={styles.tagsWrap}>
+              {blog.tags && blog.tags.map(tag => (
+                <Link
+                  to={`/blog/t/${tag.tagName}`}
+                  key={tag.tagName}
+                  style={{backgroundColor: tag.bgColor, color: tag.color}}
+                  className={styles.tagItem}
+                >
+                  <span>
+                    {`#${tag.tagName} `}
+                  </span>
+                </Link>
+              ))}
             </div>
           </div>
-        </div>
-        <Conversations></Conversations>
+        ))}
       </div>
-    )
+    )   
   }
 }
